1278124749e2f36c06d111aa7765c00b35eeac2b
Ghitha Dinan <ghitha.dinan@idn.ccb.com>
2024-02-13 18:26:20 +0700

    label add mandatory

--- src/app/shared/ui/form-label/form-label.component.ts
+++ src/app/shared/ui/form-label/form-label.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class FormLabelComponent implements OnInit {
 
   @Input() formLableClass = 'col-form-label-sm';
+  @Input() mandatory = false;
 
   constructor() {
   }
